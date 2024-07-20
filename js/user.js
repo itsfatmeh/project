b1730a36e772a781dfc1b58488bf61c213c7e760
fafa <fatmhpaydari@gmail.com>
2024-07-20 19:37:10 +0330

    part12

--- js/user.js
+++ js/user.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {});
-  });
+
+
+
+         
+     const Adduserbutton= document.getElementById("add-user-button")
+     Adduserbutton.addEventListener("click" , ()=>{
+         document.querySelector(".add-user-modal").classList.add("show")
+         document.querySelector(".add-user-modal-back").classList.remove("dis-none")
+      })
+
+      document.querySelector(".add-user-modal-back").addEventListener("click" , ()=>{
+         document.querySelector(".add-user-modal").classList.remove("show")
+         document.querySelector(".add-user-modal-back").classList.add("dis-none")
+      })
+
+});
